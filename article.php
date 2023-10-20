001e779f9ce024dec662ec20a6101a5505f65ddc
[apexdevweb] <apexdevweb@gmail.com>
2023-10-20 23:56:51 +0200

    Version 1.0

--- article.php
+++ article.php
@@ -20,14 +20,16 @@ include('includes/head.php');
             echo $errorMsg;
         }
         if (isset($publi_date_select)) {
+            if (isset($publi_img_select)) {
         ?>
-
-            <h3><?= $publi_titre_select; ?></h3>
-            <hr>
-            <p><?= $publi_contenu_select; ?></p>
-            <hr>
-            <small><?= $publi_date_select . " " . $publi_auteur_select; ?></small>
+                <h3><?= $publi_titre_select; ?></h3>
+                <hr>
+                <img src="asset/image/<?= $_SESSION['id']; ?>" style="width: 150px; height: 150px">
+                <p><?= $publi_contenu_select; ?></p>
+                <hr>
+                <small><?= $publi_date_select . " " . $publi_auteur_select; ?></small>
         <?php
+            }
         }
         ?>
     </div>
