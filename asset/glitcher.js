fbe8e9e0b84f1747e6d8da400fae20728d159d1d
[apexdevweb] <apexdevweb@gmail.com>
2023-11-22 18:50:43 +0100

    Version 1.0

---
+++ asset/glitcher.js
@@ -0,0 +1,44 @@
+const gltc = document.querySelector(".gltch");
+const titregltc = document.querySelector(".sub_title");
+const cubegltc = document.querySelector(".glitch__layers");
+
+const x = gltc.addEventListener("mouseover", () => {
+  titregltc.style.display = "flex";
+  titregltc.style.fontSize = "2.3rem";
+  cubegltc.style.display = "flex";
+});
+
+const y = gltc.addEventListener("mouseout", () => {
+  titregltc.style.display = "none";
+  cubegltc.style.display = "none";
+});
+
+const gltc2 = document.querySelector(".gltch2");
+const titregltc2 = document.querySelector(".sub_title2");
+const cubegltc2 = document.querySelector(".glitch__layers2");
+
+const z = gltc2.addEventListener("mouseover", () => {
+  titregltc2.style.display = "flex";
+  titregltc2.style.fontSize = "2.3rem";
+  cubegltc2.style.display = "flex";
+});
+
+const w = gltc2.addEventListener("mouseout", () => {
+  titregltc2.style.display = "none";
+  cubegltc2.style.display = "none";
+});
+
+const gltc3 = document.querySelector(".gltch3");
+const titregltc3 = document.querySelector(".sub_title3");
+const cubegltc3 = document.querySelector(".glitch__layers3");
+
+const k = gltc3.addEventListener("mouseover", () => {
+  titregltc3.style.display = "flex";
+  titregltc3.style.fontSize = "2.3rem";
+  cubegltc3.style.display = "flex";
+});
+
+const f = gltc3.addEventListener("mouseout", () => {
+  titregltc3.style.display = "none";
+  cubegltc3.style.display = "none";
+});
