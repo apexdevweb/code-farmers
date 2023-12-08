871896e538b7928d377a37f8fdc0cbf3b9319788
[apexdevweb] <apexdevweb@gmail.com>
2023-12-08 07:59:52 +0100

    Version 1.0

--- test/app.js
+++ test/app.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-canvas.width = 900;
+canvas.width = 1200;
 canvas.height = 700;
 
 class Cell {
@@ -11,12 +11,18 @@ class Cell {
     this.width = this.effect.width;
     this.height = this.effect.height;
     this.image = document.getElementById("projectImage");
+    this.slideX = 0;
+    this.slideY = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.ease = 0.1;
+    this.friction = 0.8;
   }
   draw(context) {
     context.drawImage(
       this.image,
-      this.x,
-      this.y,
+      this.x + this.slideX,
+      this.y + this.slideY,
       this.width,
       this.height,
       this.x,
@@ -24,7 +30,21 @@ class Cell {
       this.width,
       this.height
     );
-    context.strokeRect(this.x, this.y, this.width, this.height);
+    // context.strokeRect(this.x, this.y, this.width, this.height);
+  }
+  update() {
+    const dx = this.effect.mouse.x - this.x;
+    const dy = this.effect.mouse.y - this.y;
+    const distance = Math.hypot(dx, dy);
+    if (distance < this.effect.mouse.radius) {
+      const angle = Math.atan2(dy, dx);
+      const force = distance / this.effect.mouse.radius;
+      this.vx = force * Math.cos(angle);
+      this.vy = force * Math.sin(angle);
+    }
+    //si on change les opérateur Mathématique cela change les effet de disloquance
+    this.slideX -= (this.vx *= this.friction) + this.slideX * this.ease;
+    this.slideY -= (this.vy *= this.friction) + this.slideY * this.ease;
   }
 }
 
@@ -38,7 +58,21 @@ class Effect {
     this.cell = new Cell(this, 0, 0);
     this.imageGrid = [];
     this.createGrid();
+    this.mouse = {
+      x: undefined,
+      y: undefined,
+      radius: 150,
+    };
+    this.canvas.addEventListener("mousemove", (e) => {
+      this.mouse.x = e.offsetX;
+      this.mouse.y = e.offsetY;
+    });
+    this.canvas.addEventListener("mouseleave", (e) => {
+      this.mouse.x = undefined;
+      this.mouse.y = undefined;
+    });
   }
+
   createGrid() {
     for (let y = 0; y < this.height; y += this.cellHeight) {
       for (let x = 0; x < this.width; x += this.cellWidht) {
@@ -46,11 +80,18 @@ class Effect {
       }
     }
   }
+
   render(context) {
-    this.imageGrid.forEach((cell) => {
+    this.imageGrid.forEach((cell, i) => {
+      cell.update();
       cell.draw(context);
     });
   }
 }
 const effect = new Effect(canvas);
-effect.render(ctx);
+
+function animate() {
+  effect.render(ctx);
+  requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
