f51ebcec169a191d7d876e408cddaa0021039e34
[apexdevweb] <apexdevweb@gmail.com>
2024-01-17 06:13:59 +0100

    Version 1.0

--- mp.php
+++ mp.php
@@ -16,18 +16,22 @@ include("includes/head.php");
     <br>
     <a href="messagerie.php"><i class="fa-solid fa-arrow-left"></i>Retour</a>
     <br>
-    <div class="container">
+    <div class="container GlobalMsg">
         <section id="messages">
-            <div class="container-sm">
-                <h5>Discussion avec : <img src="asset/image/<?= $_GET['id']; ?>" style="width: 50px; height: 50px; border-radius: 50px;"></h5>
-                <hr>
+            <h5>Discussion avec : <img src="asset/image/<?= $_GET['id']; ?>" style="width: 60px; height: 60px; border-radius: 50px; border: 2px solid #fff;"></h5>
+            <hr>
+            <div class="container-msg">
                 <!--on récupère et affiche le message du destinataire-->
                 <?php
                 $recupMsg = $bdd->prepare("SELECT * FROM msgprive WHERE id_expediteur = ? AND id_destinataire = ?");
                 $recupMsg->execute(array($_GET['id'], $_SESSION['id']));
                 while ($message = $recupMsg->fetch()) {
                 ?>
-                    <p><?= $message['msg_date'] . " " ?><img src="asset/image/<?= $_GET['id']; ?>" style="width: 50px; height: 50px; border-radius: 50px;"><?= " " . $message['message']; ?></p>
+                    <div class="containerDesti">
+                        <img src="asset/image/<?= $_GET['id']; ?>">
+                        <p><?= " " . $message['message']; ?></p>
+                        <small><?= $message['msg_date'] . " " ?></small>
+                    </div>
                 <?php
                 }
                 ?>
@@ -38,15 +42,18 @@ include("includes/head.php");
                 $recupMsg->execute(array($_SESSION['id'], $_GET['id']));
                 while ($message = $recupMsg->fetch()) {
                 ?>
-                    <p><?= $message['msg_date'] . " " ?><img src="asset/image/<?= $_SESSION['id']; ?>" style="width: 50px; height: 50px; border-radius: 50px;"><?= " " . $message['message']; ?></p>
+                    <div class="containerExpe">
+                        <img src="asset/image/<?= $_SESSION['id']; ?>" style="width: 50px; height: 50px; border-radius: 50px;">
+                        <p><?= " " . $message['message']; ?></p>
+                        <small><?= $message['msg_date'] . " " ?></small>
+                    </div>
                 <?php
                 }
                 ?>
                 <!--on récupère et affiche le message de l'expéditeur FIN-->
-                <hr>
             </div>
+            <hr>
         </section>
-        <br>
         <form method="POST">
             <div class="input-group flex-nowrap">
                 <input type="text" class="form-control" placeholder="votre message" name="msg_pv">
