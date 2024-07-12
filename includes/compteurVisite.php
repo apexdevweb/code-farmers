7322ca8af47b4fc15e576f9e297363c60f3537c1
[apexdevweb] <apexdevweb@gmail.com>
2024-07-12 21:07:44 +0200

    Versions 2.0

--- includes/compteurVisite.php
+++ includes/compteurVisite.php
@@ -1,6 +1,12 @@
 <?php
+// on verifie que l'id est biens passé en URL si oui avec "
+//intval on converti n'importe quel valeur passé en paramètre d'url en nombre entier si ce n'est pas le cas c'est définie sur 0
+
+$profile_user_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+
 // Récupérer le nombre de visites par mois
-$stmt = $bdd->prepare("SELECT DATE_FORMAT(`date`, '%Y-%m-%D') AS month, COUNT(*) AS visits FROM visites GROUP BY month");
+$stmt = $bdd->prepare("SELECT DATE_FORMAT(`date`, '%Y-%m-%d') AS month, COUNT(*) AS visits FROM visites WHERE `consulted_profile_id` = :profile_user_id GROUP BY month");
+$stmt->bindParam(':profile_user_id', $profile_user_id, PDO::PARAM_INT);
 $stmt->execute();
 
 $months = [];
--- includes/visite.php
+++ includes/visite.php
@@ -1,6 +1,7 @@
 <?php
 //compteur de visite avec une condition pour vérifier que l'id de la session active est biens différent de l'id du profile à visiter
 if ($_SESSION['id'] != $_GET['id']) {
-    $stmt = $bdd->prepare("INSERT INTO visites (`date`) VALUES (CURRENT_TIMESTAMP)");
-    $stmt->execute();
+    $consultedId = $_GET['id'];
+    $stmt = $bdd->prepare("INSERT INTO visites (`date`, `consulted_profile_id`) VALUES (CURRENT_TIMESTAMP,?)");
+    $stmt->execute(array($consultedId));
 }
