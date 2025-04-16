b43ea2008a4607c738f581e6d32a96480f6f46c9
Script'Enjøyer <apexdevweb@gmail.com>
2025-04-16 16:27:11 +0200

    Version 2.0

--- messageriePro.php
+++ messageriePro.php
@@ -29,16 +29,20 @@ include("include/head.php");
         ?>
             <article class="apply__container">
                 <hgroup class="apply__title--container">
-                    <h3>Candidature pour : <?= $apply_titre ?></h3>
+                    <h3><em class="apply__infos--title">Candidature pour</em> : <?= $apply_titre ?></h3>
                     <h4>REF: <?= $apply_ref ?></h4>
                 </hgroup>
+                <div class="apply__separator"></div>
+                <em class="apply__infos--title">Infos du candidat</em>
                 <ul class="apply__infos">
                     <li><?= $apply_fname ?></li>
                     <li><?= $apply_lname ?></li>
                     <li><?= $apply_mail ?></li>
                     <li><?= $apply_tel ?></li>
-                    <li><?= $apply_descript ?></li>
                 </ul>
+                <em class="apply__infos--title">Message du candidat</em>
+                <p><?= nl2br($apply_descript) ?></p>
+                <div class="apply__separator"></div>
                 <figure class="apply__img--container">
                     <label for="applyCV">Curriculum vitae</label>
                     <a href="" download="assets/candidImg/<?= $apply_cv; ?>" id="applyCv"><img src="assets/images/dwnld.png" alt="download_cv"></a>
@@ -50,6 +54,7 @@ include("include/head.php");
         }
         ?>
     </div>
+    <br>
     <?php
     include("include/footer.php");
     ?>
